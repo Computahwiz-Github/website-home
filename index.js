a1ba4d616800c35ef91dcda358b67940b9a7e391
Computahwiz <143909731+Computahwiz-Github@users.noreply.github.com>
2025-01-20 09:06:36 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function openLinkToGithub() {
+    console.log('Clicked Span');
+    window.open("https://github.com/Computahwiz-Github/Death-Statues-Mod-1.20.X", '_blank');
+}
+
+function openLinkToSite() {
+    console.log('Clicked Span');
+    window.open("https://www.isaiah.digital", '_blank');
+}
+
+function playMeAGoodFartJamie() {
+    var audio = new Audio('./Fart.mp3');
+    audio.play();
+}
+
+function letMeHearAnotherSean() {
+    var audio = new Audio('./FartReverb.mp3');
+    audio.play();
+}
