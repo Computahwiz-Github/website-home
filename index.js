a03a6b78b6b538399e6bb24b2aba9b07d587e358
Computahwiz <143909731+Computahwiz-Github@users.noreply.github.com>
2025-01-20 11:55:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function openLinkToGithub() {
+    console.log('Clicked Span');
+    window.open("https://github.com/Computahwiz-Github/Death-Statues-Mod-1.20.X", '_blank');
+}
 
+function openLinkToSite() {
+    console.log('Clicked Span');
+    window.open("https://www.isaiah.digital", '_blank');
+}
